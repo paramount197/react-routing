cc1262ee34133d6053e1cb8c990c02c5bf3f5b5c
Arabind Dhakal <arabind_97@hotmail.com>
2021-04-22 17:49:03 +0100

    React-routing practice completed

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
+import Login from "./views/login/Login";
+import Profile from "./views/profile/Profile";
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const routes = () =>{
+  return(
+  <Router>
+    <Switch>     
+      <Route path="/login">
+        <Login />
+      </Route>
+      <Route path="/profile">
+        <Profile />
+      </Route>
+      <Route path="/">
+        <App />
+      </Route>
+    </Switch>
+  </Router>
+  )  
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    {routes()}  
+   </React.StrictMode>,
   document.getElementById('root')
 );
 
